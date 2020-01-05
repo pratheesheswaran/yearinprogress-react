352ccf437c61f27d9e5a31129d0319e6232a3692
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-01-06 01:47:55 +0530

    react part of year in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Progress } from 'antd';
 import './App.css';
+import "antd/dist/antd.css";
+import logo from './icon.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+state={
+  yearpassed:0,
+  isMobile: false
+}
+componentWillMount(){
+  this.getProgress();
+  if(this.detectmob()){
+    this.setState({isMobile:this.detectmob()})
+  }
+  setInterval(()=>{
+    this.getProgress();
+  },1000)
+  
+}
+   getProgress=()=>{
+     console.log('get progress function')
+     var now = new Date();
+     var start = new Date(now.getFullYear(), 0, 1);  // Start of this year
+     var end = new Date(now.getFullYear() + 1, 0, 1);  // End of this year
+     var done = (now-start) / (end-start);
+    this.setState({yearpassed:done});
+  }
+   detectmob=() =>{ 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+     }
+    else {
+       return false;
+     }
+   }
+  render(){
+    return(
+      <div className='main-container'>
+        <img src={logo} className='logo'></img>
+      <div className='container' style={{left:`${this.state.isMobile? '50%':'45%'}`}}>
+        
+        <h1>Year In Progress</h1>
+        {this.state.isMobile ? <Progress type="circle" percent={(this.state.yearpassed*100).toFixed(2)} size='small' strokeColor='black' /> : <Progress percent={this.state.yearpassed*100} size="small"/>}
+        
+      </div>
+      <p className='text-right'>follow <a href='http://twitter.com/yearinprogress' target='_blank'>@yearinprogress</a></p>
+      <p className='text-left'><a href='https://github.com/randomriffs' target='_blank'> @randomriffs</a></p>
+
+      </div>
+    )
+  }
 }
 
 export default App;
